dc4d84021ab940308bc03b777b41672ff927b2b9
Joshtg1104 <joshtg1104@gmail.com>
2019-05-13 16:29:02 -0500

    Whew

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+//        Exercise 2
+//        for(int count=0; count <= 100; count+=count%2){
+//            System.out.println(count);
+//            count++;
+//        }
+
+//        Exercise 3
+//        System.out.println("Enter a number of your choice");
+//        Integer number = reader.nextInt();
+//        Integer kount = 0;
+//        while(kount <= number){
+//            System.out.println(kount);
+//            kount++;
+//        }
+
+//        Exercise 4
+//        System.out.println("Enter a Secret Word");
+//        String secret = reader.next();
+//        System.out.println("Guess the Secret Word");
+//        String player = reader.next();
+//        Integer guesses = 1;
+//        while(guesses <= 5){
+//            if(!player.equals(secret)){
+//                System.out.println("Wrong! Guess Again.");
+//                player = reader.next();
+//                guesses++;
+//            }
+//            else if(guesses == 5){
+//                System.out.println("You've run out of guesses.");
+//                break;
+//            }
+//            else if(player.equals(secret)){
+//                System.out.println("Correct! Congrats, good job. " + "You made " + guesses + " guesses.");
+//                return;
+//            }
+//
+//        }
+
+    }
+}
